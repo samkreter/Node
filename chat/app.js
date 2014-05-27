e68f13931ffc1af769b8b1c44b36a58d7594ac83
Sam Kreter <sakfy6@mail.missouri.edu>
2014-05-26 20:37:25 -0500

    chat add
    added private messaging to the chat app

--- chat/app.js
+++ chat/app.js
@@ -14,7 +14,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 
-var nicknames = [];
+var users = {};
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -41,31 +41,50 @@ app.get('/',function(req,res){
 
 io.sockets.on('connection', function(socket){
 	socket.on('new user', function(data, callback){
-		if (nicknames.indexOf(data) != -1){
+		if (data in users){
 			callback(false);
 		} else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNicknames();
 		}
 	});
 	
 	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+	socket.on('send message', function(data, callback){
+		var msg = data.trim();
+		console.log('after trimming message is: ' + msg);
+		if(msg.substr(0,3) === '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind !== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind + 1);
+				if(name in users){
+					users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+					console.log('message sent is: ' + msg);
+					console.log('Whisper!');
+				} else{
+					callback('Error!  Enter a valid user.');
+				}
+			} else{
+				callback('Error!  Please enter a message for your whisper.');
+			}
+		} else{
+			io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+		}
 	});
 	
 	socket.on('disconnect', function(data){
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		delete users[socket.nickname];
 		updateNicknames();
 	});
 });
-
 
 
 //socket.broadcast.emit('new message',data);
